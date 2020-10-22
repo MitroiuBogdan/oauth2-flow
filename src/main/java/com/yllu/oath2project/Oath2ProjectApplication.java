2b409afd74acacefc635cf59457f575d0e003d1d
MitroiuBogdan <mitroiu.bogdan@gmail.com>
2020-10-22 23:56:35 +0300

    initial commit

---
+++ src/main/java/com/yllu/oath2project/Oath2ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.yllu.oath2project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oath2ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oath2ProjectApplication.class, args);
+	}
+
+}
